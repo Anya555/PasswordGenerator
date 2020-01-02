4506935ee1ee9fa6a4e8af6126b91978ac6f14f0
Anna Panas <annapanas0906@gmail.com>
2020-01-02 14:36:34 -0500

    added eventlistener

--- homework3/assets/js/main.js
+++ homework3/assets/js/main.js
@@ -1,54 +1,55 @@
 
-var generateBtn = document.querySelector("#generate");
+var generate = document.getElementById("generate");
+var passLength = document.getElementById("customRange2").value;
+var specialChar = document.getElementById("special");
+var numericChar = document.getElementById("numbers");
+var lowercaseChar = document.getElementById("lowercase");
+var uppercaseChar = document.getElementById("uppercase");
 
-var confirmSpchar = confirm("If you like your password to include special characters press 'ok'");
-var confirmNum = confirm("If you like your password to iclude numbers press 'ok'");
-var confirmLow = confirm("If you like your password to include lowercase characters press 'ok'");
-var confirmUp = confirm("If you like your pasword to include uppercase characters press 'ok'");
+var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var numbers = "0123456789";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var charType = special + numbers + lowercase + uppercase;
+var pswd; 
 
 function generatePassword() {
-  var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  var numericChar = "0123456789";
-  var lowecaseChar = "abcdefghijklmnopqrstuvwxyz";
-  var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var pswd = "";
-  if (specialChar) {
-    for (var i = 0; i >= 8 && i <= 128; i++) {
-      pswd += pswd + specialChar.charAt(Math.floor(Math.random() * specialChar.length));
-    }
-    return pswd;
+ 
+   pswd = "";
+
+  if (specialChar.checked) {
+    pswd += special;
   }
-  else if (numericChar) {
-    for (var i = 0; i >= 8 && i <= 128; i++) {
-      pswd += pswd + numericChar.charAt(Math.floor(Math.random() * numericChar.length));
-    }
-    return pswd;
+  if (numericChar.checked) {
+    pswd += numbers;
   }
-  else if (lowecaseChar) {
-    for (var i = 0; i >= 8 && i <= 128; i++) {
-      pswd += pswd + lowercaseChar.charAt(Math.floor(Math.random() * lowercaseChar.length));
-    }
-    return pswd;
+  if (lowercaseChar.checked) {
+    pswd += lowercase;
   }
-  else if (uppercaseChar) {
-    for (var i = 0; i >= 8 && i <= 128; i++) {
-      pswd += pswd + uppercaseChar.charAt(Math.floor(Math.random() * uppercaseChar.length));
-    }
-    return pswd;
-  }
+  if (uppercaseChar.checked) {
+    pswd += uppercase;
+  } 
   else {
-    alert("please select at least one charachter type");
-  }
+    alert("Please choose at least one charcter type");
   }
 
-// console.log(generatePassword);
+  for (var i = 0; i < passLength; i++) {
+    pswd += charType.charatAt(Math.floor(Math.random() * charType.length));
+  }
+  return pswd;
+}
+
 
 generate.addEventListener("click", generatePassword);
 var generatePassword = function (event) {
   event.preventDefault();
 }
 
-copy.addEventListener("click", copyToClipboard);
-var copyToClipboard = function (event) {
-  event.preventDefault();
-}
+var input = document.getElementById("password");
+var btnCopy = document.getElementById("copy");
+
+btnCopy.onclick = function () {
+  input.select();
+  document.execCommand("copy");
+};
+
